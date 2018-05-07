059c6eca11820a4fadf6233528ba91b27f73a654
cafemoca <dev@moca.cm>
2018-05-08 05:42:14 +0900

    :art: Refactor to use ByteBuffer for read client packets

--- src/main/kotlin/server/packets/client/_ClientPacket.kt
+++ src/main/kotlin/server/packets/client/_ClientPacket.kt
@@ -1,64 +1,76 @@
 package cm.moca.l1k.server.packets.client
 
-import io.ktor.cio.toByteReadChannel
-import kotlinx.coroutines.experimental.io.readRemaining
-import java.io.ByteArrayInputStream
+import kotlinx.coroutines.experimental.io.ByteBuffer
 import java.nio.charset.Charset
+import kotlin.math.min
 
 abstract class _ClientPacket(data: ByteArray) {
 
-    private val bs = ByteArrayInputStream(data)
-    private val br = bs.toByteReadChannel()
+    private val bs = ByteBuffer.wrap(data)
 
     init {
-        bs.skip(OFFSET.toLong())
+        skip(OFFSET)
     }
 
     fun reset() {
         bs.reset()
+        skip(OFFSET)
     }
 
     fun skip(skip: Int) {
-        bs.skip(skip.toLong())
+        bs.position(bs.position() + OFFSET)
     }
 
     suspend fun readInt(): Int {
-        return br.readInt() and 0xFF
+        return bs.int and 0xFF
     }
 
     suspend fun readByte(): Int {
-        return br.readByte().toInt() and 0xFF
+        return bs.get().toInt() and 0xFF
     }
 
     suspend fun readBytes(length: Int): ByteArray {
-        val temp = ByteArray(length)
-        br.readAvailable(temp)
-        return temp
+        return _readBytes(min(bs.remaining(), length))
     }
 
     suspend fun readBytes(): ByteArray {
-        return readBytes(br.readRemaining().remaining)
+        return _readBytes(bs.remaining())
+    }
+
+    private suspend fun _readBytes(length: Int): ByteArray {
+        val temp = ByteArray(length)
+        bs.get(temp)
+        return temp
     }
 
     suspend fun readShort(): Int {
-        return br.readShort().toInt() and 0xFF
+        return bs.short.toInt() and 0xFF
     }
 
     suspend fun readDouble(): Double {
-        return br.readDouble()
+        return bs.double
     }
 
     suspend fun readString(): String {
-        return readString(br.readRemaining().remaining)
+        var d = byteArrayOf()
+        var t: Byte
+        while (bs.hasRemaining()) {
+            t = bs.get()
+            if (t == 0x00.toByte()) {
+                break
+            }
+            d += t
+        }
+        return String(d, 0, d.size, CLIENT_LANGUAGE_CODE)
     }
 
     suspend fun readString(length: Int): String {
-        var d = readBytes(length)
+        var d = _readBytes(length)
         return String(d, 0, length, CLIENT_LANGUAGE_CODE)
     }
 
     companion object {
-        private val CLIENT_LANGUAGE_CODE = Charset.forName("UTF-8")
+        private val CLIENT_LANGUAGE_CODE = Charset.forName("SJIS")
         private val OFFSET = 5
     }
 
